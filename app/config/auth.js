df03ad95f778fb9568189ae35c90e6fe8a746275
Adam Thede <adam@thedetech.com>
2014-03-30 23:41:58 -0500

    Update auth.js
    Concealing my secrets :) :beers:

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,31 +3,31 @@
 module.exports = {
   'facebookAuth' : {
     'clientID'          : '420777854724886',
-    'clientSecret'      : '9694773fe668e106b7d85d4b0ee19086',
+    'clientSecret'      : '',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/facebook/callback'
   },
 
   'twitterAuth'  : {
     'consumerKey'       : 'kPgkTNikPdWCVgsXdYL0kA',
-    'consumerSecret'    : 'KvDeYuL0mZLD4s6acHsXXvOfOCOLv0HJ0iDMONfiBY',
+    'consumerSecret'    : '',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/twitter/callback'
   },
 
   'googleAuth'   : {
     'clientID'          : '10694217467-bllafubqadm9lgrt1m9ts0kaa29fpjq5.apps.googleusercontent.com',
-    'clientSecret'      : 'QetZYVKcJlLYf1ESOhc2z9lQ',
+    'clientSecret'      : '',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/google/callback'
   },
 
   'foursquareAuth' : {
     'clientID'          : 'BVGNQCJFKI0KI1JP02HXJ1OXHZTDEY5AACDNBMJIXTSMRLIG',
-    'clientSecret'      : 'NRRV4FO45LDORFYEW3JUMS5GLOY1ZIFJ2WGDTWCG1B4TY1CC',
+    'clientSecret'      : '',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/foursquare/callback'
   },
   
   'instagramAuth' : {
     'clientID'          : 'd0aed19eb08245779c136a27151112b5',
-    'clientSecret'      : '507152d957aa4b33842eb67d0e1f1b68',
+    'clientSecret'      : '',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/instagram/callback'
   }
 };
