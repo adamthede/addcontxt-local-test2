ba17b5c780e5c030ad9fc819480c8a72fb30841c
Adam Thede <adam@thedetech.com>
2014-03-30 23:46:11 -0500

    Update auth.js
    Let's try this one more time :beers:

--- app/config/auth.js
+++ app/config/auth.js
@@ -3,31 +3,31 @@
 module.exports = {
   'facebookAuth' : {
     'clientID'          : '420777854724886',
-    'clientSecret'      : '',
+    'clientSecret'      : '5555',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/facebook/callback'
   },
 
   'twitterAuth'  : {
     'consumerKey'       : 'kPgkTNikPdWCVgsXdYL0kA',
-    'consumerSecret'    : '',
+    'consumerSecret'    : '5555',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/twitter/callback'
   },
 
   'googleAuth'   : {
     'clientID'          : '10694217467-bllafubqadm9lgrt1m9ts0kaa29fpjq5.apps.googleusercontent.com',
-    'clientSecret'      : '',
+    'clientSecret'      : '5555',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/google/callback'
   },
 
   'foursquareAuth' : {
     'clientID'          : 'BVGNQCJFKI0KI1JP02HXJ1OXHZTDEY5AACDNBMJIXTSMRLIG',
-    'clientSecret'      : '',
+    'clientSecret'      : '5555',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/foursquare/callback'
   },
   
   'instagramAuth' : {
     'clientID'          : 'd0aed19eb08245779c136a27151112b5',
-    'clientSecret'      : '',
+    'clientSecret'      : '5555',
     'callbackURL'       : 'http://addcontxt.adamthede.me/auth/instagram/callback'
   }
 };
